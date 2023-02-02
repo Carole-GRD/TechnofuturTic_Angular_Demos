e9f9249f73ad6ae9cbaa7a7d452e1613dff56b03
Carole Gérard <gerard.carole@skynet.be>
2023-02-02 13:36:52 +0100

    Exo 5 : add comment

--- src/app/exos/exo5/cours/cours.component.ts
+++ src/app/exos/exo5/cours/cours.component.ts
@@ -12,23 +12,19 @@ export class CoursComponent {
     new Cours('CSS', 'Les rendre plus ou moins beaux'),
     new Cours('JavaScript', 'Les rendre dynamiques'),
     new Cours('C#', 'Un super langage orienté objet')
-
   ]  
 
-  
-
   isOpen(cour: Cours) {
-    
+    // Stocker la valeur (du booleen "isActive") du cours checké
     let isChecked: boolean;
     isChecked = cour.isActive;
 
+    // mettre toutes les valeurs à "false"
     this.cours.forEach((cour) => cour.isActive = false);
 
+    // inverser la valeur initiale du cours checké
     cour.isActive = !isChecked;
-
   }
-  
-  
 }
 
 
@@ -37,7 +33,7 @@ export class Cours {
   description: string;
   isActive: boolean;
 
-  constructor(title: string, decription: string, isActive: boolean = false) {;
+  constructor(title: string, decription: string, isActive: boolean = false) {
     this.title = title;
     this.description = decription;
     this.isActive = isActive;
